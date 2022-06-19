3c442bb6565b6dd8bbf177cd93cefc1b5b70c248
abj <abj@gmail.com>
2022-06-20 02:58:05 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,54 @@
+import "./App.css";
+import React from 'react';
+import { BrowserRouter,Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
+import MyNotes from "./screens/MyNotes/MyNotes";
+import SingleNote from "./screens/SingleNote/SingleNote";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
+import CreateNote from "./screens/SingleNote/CreateNote";
+import { useState } from "react";
+import ProfileScreen from "./screens/ProfileScreen/ProfileScreen";
+
+function App() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <BrowserRouter>
+      <Header setSearch={(s) => setSearch(s)} />
+      <main className="App">
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/register" component={RegisterScreen} />
+        <Route
+          path="/mynotes"
+          component={({ history }) => (
+            <MyNotes search={search} history={history} />
+          )}
+        />
+        <Route path="/note/:id" component={SingleNote} />
+        <Route path="/createnote" component={CreateNote} />;
+        <Route path="/profile" component={ProfileScreen} />
+        
+      </main>
+        <Footer />
+    </BrowserRouter>
+  );
+}
+/*
+<Route exact path="/" element={<LandingPage/>} />
+        <Route path="/login" element={<LoginScreen/>} />
+        <Route path="/register" element={<RegisterScreen/>} />
+        <Route
+          path="/mynotes"
+          element={({ history }) => (
+            <MyNotes search={search} history={history} />
+          )}
+        />
+        <Route path="/note/:id" element={<SingleNote/>} />
+        <Route path="/createnote" element={<CreateNote/>} />;
+        <Route path="/profile" element={<ProfileScreen />} />*/
+
+export default App;
